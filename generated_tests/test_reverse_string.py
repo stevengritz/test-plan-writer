9c581727fb6701dbf07d0c53976672fcd5fe79ff
root <root@worker>
2025-04-16 16:19:58 +0000

    feat: Add generated test scripts

--- generated_tests/test_reverse_string.py
+++ generated_tests/test_reverse_string.py
@@ -8,19 +8,29 @@ import unittest
 # Test class using unittest
 class TestReverseString(unittest.TestCase):
 
-    def test_reverse_string(self):
+    def test_reverse_string_normal(self):
         self.assertEqual(reverse_string('hello'), 'olleh')
+
+    def test_reverse_string_empty(self):
         self.assertEqual(reverse_string(''), '')
+
+    def test_reverse_string_single_char(self):
+        self.assertEqual(reverse_string('a'), 'a')
+
+    def test_reverse_string_special_chars(self):
+        self.assertEqual(reverse_string('!@#'), '#@!')
+
+    def test_reverse_string_numbers(self):
         self.assertEqual(reverse_string('12345'), '54321')
 
-    def test_reverse_string_edge_cases(self):
-        self.assertEqual(reverse_string('a'), 'a')
-        self.assertEqual(reverse_string('A'), 'A')
-        self.assertEqual(reverse_string(' '), ' ')
+    def test_reverse_string_mixed(self):
+        self.assertEqual(reverse_string('a1b2c3'), '3c2b1a')
 
-    def test_reverse_string_non_string_input(self):
-        with self.assertRaises(TypeError):
-            reverse_string(123)
+    def test_reverse_string_long_string(self):
+        self.assertEqual(reverse_string('a' * 10000), 'a' * 10000)
+
+    def test_reverse_string_unicode(self):
+        self.assertEqual(reverse_string('café'), 'éfac')
 
 # Boilerplate to run tests
 if __name__ == '__main__':
